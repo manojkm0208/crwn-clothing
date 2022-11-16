4b7d807cdf5a2590a23ee7acef3b7cd5a3774e5e
Manoj KM <manojkm@Manojs-MacBook-Pro.local>
2022-11-16 16:53:58 +0530

    updated sign-in component to authentication componrnet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Home from "./routes/home/home.component";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentiction/authentiction.component";
+
+const Shop = () => {
+  return <h1>I am the Shop page</h1>;
+};
 
 const App = () => {
-  const Shop = () => {
-    return <h1>I am the Shop page</h1>;
-  };
-
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
